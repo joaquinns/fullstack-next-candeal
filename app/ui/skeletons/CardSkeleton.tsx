32449e7b882511748d7bb7a42688b94ee9aec6ff
joaquinns <joaquindevv96@gmail.com>
2024-12-12 16:48:07 -0400

    Created card skeleton

---
+++ app/ui/skeletons/CardSkeleton.tsx
@@ -0,0 +1,6 @@
+export const CardSkeleton = () => (
+  <article className="relative h-[420px] flex flex-col justify-between w-full">
+    <div className="bg-gray-50 rounded-t h-[200px] animate-pulse"></div>
+    <div className="bg-gray-300 rounded-b h-[220px]"></div>
+  </article>
+);
