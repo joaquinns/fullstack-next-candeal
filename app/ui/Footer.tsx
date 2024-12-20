8cfb226a0e7ac9e7eca8faaf689858dd2bce587a
joaquinns <joaquindevv96@gmail.com>
2024-12-20 01:24:30 -0400

    created footer with mockup

---
+++ app/ui/Footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <footer className="flex flex-col md:flex-row items-center justify-center gap-4 w-full h-full min-h-[90px] bg-[#ca9767] text-slate-950 font-semibold">
+      <p>Olla</p>
+      <p>Olla</p>
+      <p>Olla</p>
+    </footer>
+  );
+};
