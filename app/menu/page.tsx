3bca1ef42ef94fc57919c953381636798e49f9b5
joaquinns <joaquindevv96@gmail.com>
2024-12-20 01:23:37 -0400

    added subcategory filters component

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -4,6 +4,7 @@ import { Card } from "../ui/Card";
 import { GridSearchResults } from "../ui/GridSearchResults";
 import { GridSearchResultsSkeleton } from "../ui/GridSearchResultsSkeleton";
 import { RestaurantMenu } from "../ui/RestaurantMenu";
+import { SubcategoriesFilters } from "../ui/SubcategoriesFilters";
 
 export default async function Menu({
   searchParams,
@@ -12,21 +13,24 @@ export default async function Menu({
     q?: string;
     page?: string;
     category?: string;
+    subcategory?: string;
     limit?: string;
   };
 }) {
-  const { q, page, category, limit } = await searchParams;
+  const { q, page, category, limit, subcategory } = await searchParams;
 
   return (
     <main className="relative min-h-screen mx-auto pt-16 max-w-6xl px-4 xl:px-0">
       <RestaurantMenu />
       <section className="mx-auto">
-        <div className="flex flex-col gap-4 mt-8">
+        <div className="flex flex-col gap-4 mt-4">
           {q || category ? (
             <>
-              <h2 className="text-xl font-bold">Nuestros productos</h2>
+              {category && <SubcategoriesFilters category={category} />}
+              <h2 className="text-3xl font-bold py-2">Nuestros productos</h2>
               <Suspense fallback={<GridSearchResultsSkeleton />}>
                 <GridSearchResults
+                  subcategory={subcategory}
                   query={q}
                   page={page}
                   category={category}
@@ -36,20 +40,21 @@ export default async function Menu({
             </>
           ) : (
             <>
-              <h2 className="text-xl font-bold">Los mas Pedidos</h2>
+              <h2 className="text-3xl font-bold">Los mas Pedidos</h2>
               <div
                 style={{
                   display: "grid",
                   gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
                   gap: "1rem",
+                  margin: "2rem 0",
                 }}
               >
                 {
                   // crear componente para mas vendidos?
                 }
-                <Card />
-                <Card />
-                <Card />
+                <Card id="1" />
+                <Card id="2" />
+                <Card id="3" />
               </div>
             </>
           )}
