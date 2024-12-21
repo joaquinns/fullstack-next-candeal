81c33d0aea87111a614a9c00c8907102637554a2
joaquinns <joaquindevv96@gmail.com>
2024-12-21 17:38:10 -0400

    refactor: simplify useScroll hook and improve visibility detection logic

--- app/hooks/useScroll.tsx
+++ app/hooks/useScroll.tsx
@@ -1,47 +1,31 @@
 import { RefObject, useEffect, useRef, useState } from "react";
 
-interface ObserverOptions {
+interface UseScrollOptions {
   root?: Element | null;
   rootMargin?: string;
   threshold?: number | number[];
   oneTime?: boolean;
 }
 
-/**
- * Hook para detectar si un elemento está visible en el viewport.
- * @param options Opciones para el IntersectionObserver (root, rootMargin, threshold)
- * @returns [ref, isIntersecting] Retorna un ref para asignar al elemento y un booleano indicando si es visible
- */
 export const useScroll = (
-  options: ObserverOptions = {
-    root: null,
-    rootMargin: "0px",
-    threshold: 0.1,
-    oneTime: false,
-  }
+  options: UseScrollOptions = {}
 ): [RefObject<Element | null>, boolean] => {
   const elementRef = useRef<Element | null>(null); // Permite que sea null inicialmente
   const [isIntersecting, setIsIntersecting] = useState(false);
 
   useEffect(() => {
-    let timeoutId: NodeJS.Timeout | null = null;
-
     const observer = new IntersectionObserver(([entry], obs) => {
-      const { isIntersecting } = entry;
+      if (entry.isIntersecting) {
+        setIsIntersecting(true);
 
-      // Usamos debounce para evitar actualizaciones rápidas consecutivas
-      if (timeoutId) clearTimeout(timeoutId);
-      timeoutId = setTimeout(() => {
-        setIsIntersecting((prev) => {
-          if (prev !== isIntersecting) {
-            if (isIntersecting && options.oneTime) {
-              obs.unobserve(entry.target);
-            }
-            return isIntersecting;
-          }
-          return prev;
-        });
-      }, 50); // Debounce de 50ms
+        // Si `oneTime` está activado, deja de observar el elemento
+        if (options.oneTime) {
+          obs.unobserve(entry.target);
+        }
+      } else if (!options.oneTime) {
+        // Si no es `oneTime`, actualiza el estado como false cuando sale de la vista
+        setIsIntersecting(false);
+      }
     }, options);
 
     const currentElement = elementRef.current;
@@ -50,10 +34,7 @@ export const useScroll = (
     }
 
     return () => {
-      if (timeoutId) clearTimeout(timeoutId);
-      if (currentElement) {
-        observer.unobserve(currentElement);
-      }
+      if (currentElement) observer.unobserve(currentElement);
       observer.disconnect();
     };
   }, [options]);
