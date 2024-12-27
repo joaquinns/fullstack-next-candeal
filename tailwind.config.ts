703fcb49589b5f1f0b6175bddea1ec7a0d12311c
joaquinns <joaquindevv96@gmail.com>
2024-12-27 18:43:02 -0400

    feat: add safelist for Tailwind CSS to improve utility class handling

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,14 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  safelist: [
+    "translate-x-0",
+    "translate-x-full",
+    "opacity-100",
+    "opacity-0",
+    "scale-100",
+    "scale-20",
+  ],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
